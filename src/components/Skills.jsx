b1736038f75babf69f403e2163f4a4874ebd6cfd
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-02-22 14:31:19 +0100

    Skills animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,6 +17,7 @@ function Skills() {
     ])
 
     const [levels, setLevels] = useState([0, 0, 0, 0, 0])
+    const [levelsLoaded, setLoaded] = useState(false)
 
     useEffect(() => {
         let dummyLevels = [90, 80, 70, 70, 70]
@@ -28,11 +29,71 @@ function Skills() {
             })
 
             setLevels(newLevels)
-            if (newLevels === dummyLevels) { console.log('Same levels'); clearInterval(myInt) }
+            let allSame = false
+            const compareArrays = () => {
+                let sameOnes = 0
+                dummyLevels.forEach((level, i) => {
+                    newLevels.forEach((lev, j) => {
+                        if (lev === level && i == j) {
+                            sameOnes += 1
+                        }
+                    })
+                    allSame = sameOnes === newLevels.length ? true : false
+                })
+                return allSame
+            }
+            if (compareArrays()) { console.log('Same levels'); setLoaded(true); clearInterval(myInt) }
             otherLevels = newLevels
         }, 100)
 
     }, [])
+
+    useEffect(() => {
+        let dummyLevels = [90, 80, 70, 70, 70]
+        let otherLevels = [0, 0, 0, 0, 0]
+
+        let levelsShining = [...document.querySelectorAll('.shining-prog')]
+
+        levelsShining.forEach((lev, i) => {
+            lev.style.backgroundPosition = `0px 50%`
+        })
+
+        if (levelsLoaded) {
+            // console.log(levelsLoaded)
+            let myInt = setInterval(() => {
+                let newLevels = otherLevels.map((lev, i) => {
+                    if (i === 0) levelsShining[i].style.background = `linear-gradient(to right, lightblue ${10 - lev}%, white ${lev}%, lightblue 100%)`
+
+                    if (otherLevels[i] == dummyLevels[i]) { return Number(lev) }
+                    else { return Number(lev) + 5 }
+                })
+                let allSame = false
+                const compareArrays = () => {
+                    let sameOnes = 0
+                    for (let i = 0; i < dummyLevels.length; i++) {
+                        for (let j = 0; j < newLevels.length; j++) {
+                            if (dummyLevels[i] === newLevels[j] && i == j) {
+                                sameOnes += 1
+                                break
+                            }
+                        }
+                        allSame = sameOnes === newLevels.length ? true : false
+                    }
+                    return allSame
+                }
+                if (compareArrays()) {
+                    console.log('Same dummy levels');
+                    newLevels.forEach((lev, i) => {
+                        // console.log(levelsShining[i])
+                        // levelsShining[i].style.width = `${Number(lev) * 5}px`
+                        levelsShining[i].style.background = i === 0 ? 'lightblue' : 'rgb(78, 102, 109)'
+                    })
+                    clearInterval(myInt)
+                }
+                otherLevels = newLevels
+            }, 100)
+        }
+    }, [levelsLoaded])
 
     const { attributes, listeners, setNodeRef, transform } = useDraggable({
         id: 6,
@@ -64,8 +125,19 @@ function Skills() {
                             <div key={skill.name} className="item">
                                 <h3 className="level-title">{skill.name}</h3>
                                 <div className="level-bar progress" style={{ zIndex: 10, height: '7px' }}>
-                                    <span className="progress-bar-value" ref={skillRefs[i]}
-                                        style={{ backgroundColor: i === 0 ? 'lightblue' : "rgb(78, 102, 109)", width: skill.level + "%", borderRadius: '20px', height: '7px' }} id={skill.name}></span>
+                                    <span className="shining-prog progress-bar-value" ref={skillRefs[i]}
+                                        style={{
+                                            // backgroundSize: '20px',
+                                            // backgroundPosition: !levelsLoaded && '0px 0px',
+                                            background: i === 0 ? 'lightblue' : 'rgb(78, 102, 109)',
+
+                                            width: skill.level + "%", borderRadius: '20px', height: '7px'
+                                        }} id={skill.name}></span>
+                                    {/* <a style={{
+                                        zIndex: 10, height: '7px',
+                                        background: i === 0 ? 'linear-gradient(to right, lightblue 20%, #FFF 40%, #FFF 60%, lightblue 80%)'
+                                            : "linear-gradient(to right, rgb(78, 102, 109) 20%, #FFF 40%, #FFF 60%,rgb(78, 102, 109)  80%)",
+                                    }} className="shining-prog"></a> */}
                                 </div>
                             </div>)
                     })}
