af5c4b07dd8cf157eb80a6aa90204c9b3e13c709
Gustaf26 <gustavo.catalasverdrup@gmail.com>
2025-06-14 09:34:07 +0200

    (Design + Functionality > GithubData): Updating dinamiccaly the repos number and commits of the page. Adding some styles

--- src/sections/container-one/GithubData.jsx
+++ src/sections/container-one/GithubData.jsx
@@ -1,20 +1,65 @@
 
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 
 import { GithubContext } from '../../contexts/GithubContext.jsx'
 
+const DataWheel = (data) => {
+    return (<span class="loader">Repositories</span>)
+}
 
 const GithubData = () => {
 
     const { gitUserData, gitRepoEvents } = useContext(GithubContext)
+    const [commits, setCommits] = useState(0)
+    const [repos, setRepos] = useState(0)
 
-    console.log(gitUserData, gitRepoEvents)
+    useEffect(() => {
+
+        if (gitUserData) {
+            let index = 0
+            const reposInterval = setInterval(() => {
+                if (index <= Number(gitUserData.data?.public_repos))
+                    setRepos(index)
+                else clearInterval(reposInterval)
+                index += 1
+            }, 100)
+        }
+
+    }, [gitUserData])
+
+    useEffect(() => {
+
+
+        if (gitRepoEvents) {
+
+            let events = gitRepoEvents.data
+            let allCommits = events.map((a) => {
+                return (a.payload.commits.length)
+            })
+            let commitsQty = allCommits.reduce((a, b) => a + b)
+
+            let index = 0
+            const commitsInterval = setInterval(() => {
+                if (index <= commitsQty)
+                    setCommits(index)
+                else clearInterval(commitsInterval)
+                index += 1
+            }, 100)
+        }
+
+    }, [gitRepoEvents])
 
     return (<div id="github-data-container">
-        <span class="loader">Repositories</span>
-        <span class="loader">Commits</span>
-        <span class="loader"></span>
-        <span class="loader"></span>
+        <div className="loader-container">
+            <h6>Repositories</h6>
+            <span className="loader">{repos && repos}</span></div>
+        <div className="loader-container">
+            <h6>Commits In This Page</h6>
+            <span className="loader">{commits && commits}</span></div>
+        <div className="loader-container">
+            <span className="loader"></span></div>
+        <div className="loader-container">
+            <span className="loader"></span></div>
     </div>)
 }
 
